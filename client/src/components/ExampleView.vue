ecb0a0366dbfda9d2ae5f885dc86807da617549d
Fei NIE <feinie@Feis-MacBook-Pro-754.local>
2020-08-07 20:07:33 +0800

    add feature to exampleview

--- client/src/components/ExampleView.vue
+++ client/src/components/ExampleView.vue
@@ -16,21 +16,40 @@
     <span>Evidence</span>
     <span class="badge badge-relevance m-1">R</span>
     <span>Relevance</span>
+    <br />
+    <span>Concreteness (from low to high)</span>
+    <span class="badge badge-concreteness m-1" style="opacity:0.2">C</span>
+    <span class="badge badge-concreteness m-1" style="opacity:0.5">C</span>
+    <span class="badge badge-concreteness m-1" style="opacity:1">C</span>
+    <span class="ml-4">Eloquence (from low to high)</span>
+    <span class="badge badge-eloquence m-1" style="opacity:0.2">El</span>
+    <span class="badge badge-eloquence m-1" style="opacity:0.5">El</span>
+    <span class="badge badge-eloquence m-1" style="opacity:1">El</span>
 
     <ul class="list-group" id="examplelist">
+      <br />
       <li
         class="list-group-item d-flex justify-content-between align-items-center"
         v-for="item in items"
         :key="item.content"
       >
-        {{item.content}}
-        <div class="row col-2">
-          <span class="badge badge-claim m-1" v-if="item.is_claim!=='0'">{{item.claim_type}}</span>
-          <span class="badge badge-logos m-1" v-if="item.logos!=='0'">L</span>
-          <span class="badge badge-pathos m-1" v-if="item.pathos!=='0'">P</span>
-          <span class="badge badge-ethos m-1" v-if="item.ethos!=='0'">E</span>
-          <span class="badge badge-evidence m-1" v-if="item.evidence!=='0'">Ev</span>
-          <span class="badge badge-relevance m-1" v-if="item.relevance!=='0'">R</span>
+        <div class="row">
+          <div class="col-8">{{item.content}}</div>
+          <div class="col-2">
+            <span class="badge badge-claim m-1" v-if="item.is_claim!=='0'">{{item.claim_type}}</span>
+            <span class="badge badge-logos m-1" v-if="item.logos!=='0'">L</span>
+            <span class="badge badge-pathos m-1" v-if="item.pathos!=='0'">P</span>
+            <span class="badge badge-ethos m-1" v-if="item.ethos!=='0'">E</span>
+            <span class="badge badge-evidence m-1" v-if="item.evidence!=='0'">Ev</span>
+            <span class="badge badge-relevance m-1" v-if="item.relevance!=='0'">R</span>
+          </div>
+          <div class="col-2">
+            <span
+              class="badge badge-concreteness m-1"
+              v-bind:style="{opacity:((item.concreteness-0.15)*10)}"
+            >C</span>
+            <span class="badge badge-eloquence m-1" v-bind:style="{opacity:item.eloquence/2}">El</span>
+          </div>
         </div>
       </li>
     </ul>
@@ -43,6 +62,7 @@ export default {
   name: "SelectView",
   data() {
     return {
+      opacity: { opacity: 0.5 },
       name: json["Dating-16"][0]["reply-info"][0]["replyer_name"],
       items: json["Dating-16"][0]["reply-info"][0]["reply_contents"],
     };
@@ -82,6 +102,14 @@ export default {
 }
 .badge-relevance {
   background-color: rgb(224, 92, 92);
+  color: white;
+}
+.badge-concreteness {
+  background-color: rgb(1, 83, 19);
+  color: white;
+}
+.badge-eloquence {
+  background-color: rgb(1, 13, 83);
   color: white;
 }
 </style>
