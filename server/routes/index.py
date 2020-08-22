42abbd8e40d2f3cda4a5ad8842d126b4b433bfa4
xingbow <wangxbzb@foxmail.com>
2020-08-22 22:46:53 +0800

    add: classification models

--- server/routes/index.py
+++ server/routes/index.py
@@ -6,6 +6,7 @@ from flask import Flask, request, redirect, jsonify, send_from_directory, make_r
 from flask_cors import *
 from server import app
 from functools import wraps
+import pandas as pd
 import hashlib
 import random
 import time
@@ -15,6 +16,45 @@ import json
 import requests
 import subprocess
 from math import sqrt
+from server.dataprocessing.sentenceCls import SentenceEmbedder as se
+import torch
+import transformers as ppb 
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+sentenceEmbedder = se()
+
+
+
+import joblib
+
+
+def load_models():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # argument model
+    with open(os.path.join(dir_path, "../models/argument_model.joblib"), 'rb') as f:
+        arg_model = joblib.load(f)
+
+    with open(os.path.join(dir_path, "../models/ml_argument.joblib"), 'rb') as f:
+        ml_arg = joblib.load(f)
+
+    # claim type model
+    with open(os.path.join(dir_path, "../models/claim_model.joblib"), 'rb') as f:
+        claim_model = joblib.load(f)
+
+    with open(os.path.join(dir_path, "../models/ml_claims.joblib"), 'rb') as f:
+        ml_claim = joblib.load(f)
+
+    # premise type model
+    with open(os.path.join(dir_path, "../models/premise_model.joblib"), 'rb') as f:
+        premise_model = joblib.load(f)
+
+    with open(os.path.join(dir_path, "../models/ml_premises.joblib"), 'rb') as f:
+        ml_premise = joblib.load(f)
+
+    return [[arg_model, ml_arg], [claim_model, ml_claim], [premise_model, ml_premise]]
 
 # app = Flask(__name__)
 # CORS(app, supports_credentials=True)
@@ -33,7 +73,7 @@ def register():
 @app.route('/uploadInput', methods=['POST','GET']) 
 def uploadInput():
     temp = json.dumps(request.data.decode("utf-8"))
-    print(temp, type(temp))
+        
     return temp
 
 
