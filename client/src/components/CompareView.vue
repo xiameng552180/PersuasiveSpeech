821daa3581712b8c34c1606872458df552825a90
Fei NIE <feinie@Feis-MacBook-Pro-754.local>
2020-09-04 12:19:10 +0800

    exampleview highlight selected

--- client/src/components/CompareView.vue
+++ client/src/components/CompareView.vue
@@ -976,7 +976,7 @@ export default {
             DataService.selectIDIndex = []; //clear index
 
             PipeService.$emit(PipeService.UPDATE_SELECTVIEW);
-            PipeService.$emit(PipeService.UPDATE_EXAMPLEVIEW);
+            // PipeService.$emit(PipeService.UPDATE_EXAMPLEVIEW);
             PipeService.$emit(PipeService.UPDATE_COMPAREVIEW);
           }
           //console.log("IDARRAY: ", this.selectIDarray, this.selectIDIndex, this.examples.map((d) => d.id));
