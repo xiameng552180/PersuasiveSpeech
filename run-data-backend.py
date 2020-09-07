59e2766dfd821b2d7f37862bf4a1c5322f83b98d
xiameng552180 <iris.xia@connect.ust.hk>
2020-09-07 14:08:39 +0800

    publish

--- run-data-backend.py
+++ run-data-backend.py
@@ -4,5 +4,5 @@ from server import app
 from gevent.pywsgi import WSGIServer
 # app.config['STATIC_FOLDER'] = './client/assets'
 
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('', 5555), app)
 http_server.serve_forever()
